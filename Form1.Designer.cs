9a43a24e4a3f5ed0a0ccfc7283c41f2decfae3b4
Mohoshi Haque <46513655+MohoshiHaque@users.noreply.github.com>
2021-03-07 21:33:23 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnName = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.IdBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(243, 147);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(69, 15);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Your Name:";
+            this.Name.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(318, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnName
+            // 
+            this.btnName.Location = new System.Drawing.Point(345, 187);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(88, 24);
+            this.btnName.TabIndex = 2;
+            this.btnName.Text = "Show Name";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(318, 234);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // IdBtn
+            // 
+            this.IdBtn.Location = new System.Drawing.Point(350, 281);
+            this.IdBtn.Name = "IdBtn";
+            this.IdBtn.Size = new System.Drawing.Size(82, 23);
+            this.IdBtn.TabIndex = 5;
+            this.IdBtn.Text = "Show ID";
+            this.IdBtn.UseVisualStyleBackColor = true;
+            this.IdBtn.Click += new System.EventHandler(this.IdBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IdBtn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Name);
+            
+            this.Text = "Company";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button IdBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            MessageBox.Show(name);
+        }
+
+        private void IdBtn_Click(object sender, EventArgs e)
+        {
+            string id1 = textBox2.Text;
+            MessageBox.Show(id1);
+        }
+    }
+}
